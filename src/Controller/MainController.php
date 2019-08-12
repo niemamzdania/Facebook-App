7c431496395eb0fd0ce3f86ada0bddf5f15fd48e
Mateusz <milewskimateusz28@gmail.com>
2019-08-12 13:12:16 +0200

    Modify templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use App\Entity\Quests;
 use App\Form\EditQuestFormType;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="test")
+     *  @IsGranted("ROLE_ADMIN")
      */
     public function mainpage()
     {
