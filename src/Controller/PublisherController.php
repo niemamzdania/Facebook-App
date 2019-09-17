798d804dff1e7e761c812837c47995b380a63cab
Przemek <przemek.prusik.96@gmail.com>
2019-09-17 12:54:02 +0200

    Begin work over chat

--- src/Controller/PublisherController.php
+++ src/Controller/PublisherController.php
@@ -4,10 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Users;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mercure\Publisher;
 use Symfony\Component\Mercure\Update;
@@ -16,6 +16,7 @@ class PublisherController extends AbstractController
 {
     /**
      * @Route("/publish/{topic}", name="publisher", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
      */
     public function publish($topic, Publisher $publisher, Request $request, Session $session)
     {
@@ -28,10 +29,12 @@ class PublisherController extends AbstractController
 
     /**
      * @Route("/chat", name="chat")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
      */
     public function chat(Session $session)
     {
+        $users = $this->getDoctrine()->getRepository(Users::class)->findAllUsers();
 
-        return $this->render('chat/index.html.twig', ['topic' => '1e9']);
+        return $this->render('chat/index.html.twig', ['topic' => '1e9','users' => $users]);
     }
 }
