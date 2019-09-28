4898d333d0f9e55d4875e3d492e76d2c76849386
niemamzdania <przemek.prusik.96@gmail.com>
2019-09-28 11:12:04 +0200

    Working over translation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Conversations;
-use App\Entity\Users;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +15,20 @@ class MainController extends AbstractController
      * @Route("/", name="main_page")
      * @IsGranted("IS_AUTHENTICATED_FULLY")
      */
-    public function mainpage(ObjectManager $manager)
+    public function mainpage(Request $request)
     {
         return $this->render('mainpage/main.html.twig');
+    }
+
+    /**
+     * @Route("/changeLanguage/{lang}", name="change_lang")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function changeLanguage(Request $request, $lang)
+    {
+        $session = $request->getSession();
+        $session->set('locale', $lang);
+
+        return $this->redirectToRoute('main_page');
     }
 }
---
+++ src/EventSubscriber/LanguageSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LanguageSubscriber implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        $request->setLocale($request->getSession()->get('locale'));
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
